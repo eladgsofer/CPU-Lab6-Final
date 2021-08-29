bf580320a81587d664fafcc43bdb33e35b069652
eladsofer879 <36042562+eladsofer879@users.noreply.github.com>
2021-08-29 21:15:00 +0300

    C# terminal

---
+++ C# Client side/FormChat.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TerminalPC
+{
+    public partial class FormChat : Form
+    {
+        public FormChat()
+        {
+            InitializeComponent();
+            Home.port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                
+                try
+                {
+                    if (!Home.port.IsOpen)
+                    {
+                        Home.port.Open();
+                    }
+
+                    Home.port.Write(textBox1.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+                textBox1.Text = "";
+            } 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string indata;
+            SerialPort sp = (SerialPort)sender;
+            System.Threading.Thread.Sleep(500);
+
+            if (!Home.port.IsOpen)
+            {
+                Home.port.Open();
+            }
+
+            indata = Home.port.ReadExisting();
+
+            // Clean the buffer
+            MessageBox.Show("[" + DateTime.Now + "]: " + indata);
+        }
+
+        private void FormChat_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ C# Client side/Home.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace TerminalPC
+{
+    public partial class Home : Form
+    {
+        public static SerialPort port;
+        Boolean firstConnection = true;
+        public static Parity parityTemp;
+        public static StopBits stopbitsTemp;
+        public static string comTemp;
+        public static int baudTemp;
+        
+        public Home()
+        {
+            InitializeComponent();
+            comboBoxBaud.SelectedIndex = 1;
+            comboBoxCOM.SelectedIndex = 0;
+            comboBoxParity.SelectedIndex = 0;
+            comboBoxSTPBIT.SelectedIndex = 0;
+            
+        }
+
+        private void ComboBoxCOM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Connect + update parameters
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            
+            if (firstConnection) // For first connection there are default params
+            {
+                port = new SerialPort(comboBoxCOM.SelectedItem.ToString(),
+                                     int.Parse(comboBoxBaud.SelectedItem.ToString()),
+                                     (Parity)comboBoxParity.SelectedIndex,
+                                     8,
+                                     StopBits.One);
+                firstConnection = false;
+            }
+
+
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                }
+
+
+                MessageBox.Show("Connected");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        //Open chat mode window
+        private void ButtonChat_Click(object sender, EventArgs e)
+        {
+            FormChat fc = new FormChat();
+            fc.ShowDialog();
+        }
+
+        ////Open file transfer window
+        private void ButtonFile_Click(object sender, EventArgs e)
+        {
+            FormFileTransfer ft = new FormFileTransfer();
+            ft.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonFile_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+    }   
+}
